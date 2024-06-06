8b3c24a7820ace2dc07f37dd431b47a458d4f30e
Björn Bergdahl <bjorn.bergdahl@2xper.se>
2024-06-06 22:34:15 +0200

    Added democode

--- test.py
+++ test.py
@@ -1,1 +1,21 @@
-print(2+2)
+
+foodlist = []
+with open("food.txt") as myfile:
+    foodlist = myfile.readlines()
+foodstuff = set()
+for line in foodlist:
+    a, _ = line.split(':')
+    foodstuff.add(a)
+
+print(foodstuff)
+
+raw_foodcount = len(foodlist)
+foodcount = len(foodstuff)
+print('total: ', raw_foodcount," and unique: " ,foodcount)
+
+indexed_foodstuff = zip(range(foodcount), foodstuff)
+food_dict = dict()
+for each_tuple in indexed_foodstuff: 
+    print(each_tuple)
+    food_dict.update([each_tuple])
+
